50f9a2b16ffe45093dec973d5e7b55daf7826000
Sizimon <siizmonsus@gmail.com>
2022-11-17 22:08:15 +0000

    Made a loop that promopts to input data again if the data submitted was invalid

--- run.py
+++ run.py
@@ -16,15 +16,22 @@ def get_sales_data():
     '''
     Get sales figures input from the user
     '''
-    print('Please enter sales data from the last market.')
-    print('Data should be six numbers, seperated by commas.')
-    print('Example: 10, 20, 30, 40, 50, 60\n')
+    while True:
 
-    data_str = input('Enter your data here: ')
-    print(f'The data provided is {data_str}')
+        print('Please enter sales data from the last market.')
+        print('Data should be six numbers, seperated by commas.')
+        print('Example: 10, 20, 30, 40, 50, 60\n')
 
-    sales_data = data_str.split(',')
-    validate_data(sales_data)
+        data_str = input('Enter your data here: ')
+        print(f'The data provided is {data_str}')
+
+        sales_data = data_str.split(',')
+        validate_data(sales_data)
+
+        if validate_data(sales_data):
+            print('Data is valid!')
+            break 
+    return sales_data
 
 def validate_data(values):
     '''
@@ -33,12 +40,15 @@ def validate_data(values):
     or if there arent exactly 6 values.
     '''
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f'Exactly 6 values required, you provided {len(values)}'
             )
     except ValueError as e:
             print(f'Invalid data: {e}, Please try again.\n')
+            return False
+    return True
 
-get_sales_data()
+data = get_sales_data()
  
