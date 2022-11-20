41691513495a552021fe7bda979c17905e52b2c2
Sizimon <siizmonsus@gmail.com>
2022-11-20 18:16:42 +0000

    created function that takes on the role of 2 previous functions

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
+from pprint import pprint
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -54,14 +54,16 @@ def validate_data(values):
     return True
 
 
-def update_sales_worksheet(data):
+def update_worksheet(data, worksheet):
     '''
-    Update sales worksheet, add new row with the list data provided.
+    Recieves a list of integers to be updated to the worksheet
+    Update relevant worksheet with the data provided.
     '''
-    print('Updating sales worksheet...\n')
-    sales_worksheet = SHEET.worksheet('sales')
-    sales_worksheet.append_row(data)
-    print('Sales worksheet updated.\n')
+    print(f'Updating {worksheet} worksheet...\n')
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f'{worksheet} worksheet updated.\n')
+
 
 
 def calculate_surplus_data(sales_row):
@@ -84,25 +86,15 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
-def update_surplus_worksheet(data):
-    '''
-    Update surplus worksheet, add new row with the list data provided.
-    '''
-    print('Updating surplus worksheet...\n')
-    surplus_worksheet = SHEET.worksheet('surplus')
-    surplus_worksheet.append_row(data)
-    print('Surplus worksheet updated.\n')
-
-
 def main():
     '''
     Run all program functions
     '''
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, 'sales')
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, 'surplus')
  
 
 print('Welcome to Love Sandwiches Data Automation')
